1608573f5c2fe7b5398394abe8994a91d96fcfb7
KULDEEP <90776144+kuldeep1A@users.noreply.github.com>
2022-12-18 19:51:45 +0530

    Create data.py

---
+++ Quiz Game/data.py
@@ -0,0 +1,100 @@
+question_data = [
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Pythagorean theorem states that the square of the hypotenuse is equal to the product of"
+                    " the squares of the other two sides.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A universal set, or a set that contains all sets, exists.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "111,111,111 x 111,111,111 = 12,345,678,987,654,321",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The &#039;Squaring the Circle&#039; problem is solvable.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "E = MC3",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "In Topology, the complement of an open set is a closed set.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The sum of any two odd integers is odd.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "You can square root a negative number with an imaginary number &quot;i&quot;.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A scalene triangle has two sides of equal length.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "An equilateral triangle always has every angle measuring 60&deg;.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
+# question_data = [
+#     {"question": "A slug's blood is green.", "correct_answer": "True"},
+#     {"question": "The loudest animal is the African Elephant.", "correct_answer": "False"},
+#     {"question": "Approximately one quarter of human bones are in the feet.", "correct_answer": "True"},
+#    {"question": "The total surface area of a human lungs is the size of a football pitch.", "correct_answer": "True"},
+#     {"question": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+#                  "you are free to take it home to eat.", "correct_answer": "True"},
+#     {"question": "In London, UK, if you happen to die in the House of Parliament, "
+#                  "you are entitled to a state funeral.", "correct_answer": "False"},
+#     {"question": "It is illegal to pee in the Ocean in Portugal.", "correct_answer": "True"},
+#     {"question": "You can lead a cow down stairs but not up stairs.", "correct_answer": "False"},
+#     {"question": "Google was originally called 'Back-rub'.", "correct_answer": "True"},
+#     {"question": "Buzz Aldrin's mother's maiden name was 'Moon'.", "correct_answer": "True"},
+#     {"question": "No piece of square dry paper can be folded in half more than 7 times.", "correct_answer": "False"},
+#     {"question": "A few ounces of chocolate can to kill a small dog.", "correct_answer": "True"}
+# ]
+#
